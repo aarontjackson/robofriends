8a0ffaff2953e31616561e75b7ef6f0a5d658027
Aaron Jackson <aarontjackson@outlook.com>
2020-01-25 19:41:15 -0500

    added robot image from RoboHash API

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,13 @@ import React from 'react';
 const Card = () => {
     return (
         <div>
-            <img src="" alt=""/>
+            <img src="https://robohash.org/Aaron?200x200" alt="robots"/>
             <div>
                 <h2>Name</h2>
                 <p>Email@email.com</p>
             </div>
         </div>
-    )
+    );
 }
+
+export default Card;
