e56dc59c9990e00e952c6d09e866f9b890815ae4
Aaron Jackson <aarontjackson@outlook.com>
2020-01-25 19:35:38 -0500

    created Card.js file with basic temporary hard coded values

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <div>
+                <h2>Name</h2>
+                <p>Email@email.com</p>
+            </div>
+        </div>
+    )
+}
