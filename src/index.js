b163d939059c2bf93d986ca061adff3b07fe022c
Aaron Jackson <aarontjackson@outlook.com>
2020-01-25 19:57:35 -0500

    created dynamic properties for each individual card component using robot.js file

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import { robots } from './robots'
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
     </div>
         
         
