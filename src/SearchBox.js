ca9eeb4342df0e0e42c9026714399469ec9a3547
Aaron Jackson <aarontjackson@outlook.com>
2020-01-26 20:46:38 -0500

    onchange event function added to search box component and app.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return (
         <div>
         <input className= 'pa3 ba b--green bg-lightest-blue'
         type='search' 
-        placeholder='search robots'>
+        placeholder='search robots'
+        onChange={searchChange}
+        >
+       
         </input>
         </div>
     );
