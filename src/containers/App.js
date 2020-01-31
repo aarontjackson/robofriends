7ceee0c792119f5051fd5bc54be572be64ba2cde
Aaron Jackson <aarontjackson@outlook.com>
2020-01-31 16:46:46 -0500

    app.js did not render with correct material

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,9 +30,7 @@ class App extends Component {
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
         })
-        return !robots.length ?
-        <h1>Loading</h1> :
-        (
+        return(
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
