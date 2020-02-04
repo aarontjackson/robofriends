2c760474839e795613b4daa79f7cbd7513907be8
Aaron Jackson <aarontjackson@outlook.com>
2020-02-04 16:00:40 -0500

    Updates

---
+++ precache-manifest.57aae5106e88f2787836072557e3b310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1454cf3365a7100e8305fe1cea4d3318",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2e0a2e756187ec2891a7",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dad2279829d32842d7bc",
+    "url": "/robofriends/static/css/main.40e44af8.chunk.css"
+  },
+  {
+    "revision": "2e0a2e756187ec2891a7",
+    "url": "/robofriends/static/js/2.4f74f85a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.4f74f85a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dad2279829d32842d7bc",
+    "url": "/robofriends/static/js/main.94c69b1f.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
