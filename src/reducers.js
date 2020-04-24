8b7fbb4b5b1ceb3d47fc67637f97646df91d0a9c
Aaron Jackson <aarontjackson@outlook.com>
2020-04-24 12:43:32 -0400

    include a failed switch case that returns an error and is pending false

--- src/reducers.js
+++ src/reducers.js
@@ -22,7 +22,11 @@ export const requestRobots = (state=intialState, action={}) => {
     switch(action.type) {
         case REQUEST_ROBOTS_PENDING:
             return Object.assign({}, state, { isPending: true })
-        case    REQUEST_ROBOTS_SUCCESS:
+        case REQUEST_ROBOTS_SUCCESS:
             return Object.assign({}, state, { robots: action.payload, isPending: false }) 
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
+        default:
+            return state;
     }
 }
