b65204efa79869a9e9885ffaf5319042aec95fc5
Aaron Jackson <aarontjackson@outlook.com>
2020-04-24 13:05:25 -0400

    include combineReducers into the import redux code line and create a rootReducer for all the reducers in the store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider} from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './App'
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots})
 const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
