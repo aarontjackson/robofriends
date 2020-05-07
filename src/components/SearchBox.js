69ccae437459766e8c187e01e398dc48b86c94d6
Aaron Jackson <aarontjackson@outlook.com>
2020-05-07 11:33:57 -0400

    updated searchbox input tag and switch service worker to register

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,6 +6,7 @@ const SearchBox = ({ searchChange }) => {
     return (
         <div>
         <input className= 'pa3 ba b--green bg-lightest-blue'
+        aria-label="A place for robots to meet"
         type='search' 
         placeholder='search robots'
         onChange={searchChange}
