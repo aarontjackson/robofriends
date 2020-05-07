2c0152145c7d96eae0cd86e504162881f8bfd672
Aaron Jackson <aarontjackson@outlook.com>
2020-05-07 12:28:17 -0400

    A quick update

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({ searchChange }) => {
     return (
         <div>
         <input className= 'pa3 ba b--green bg-lightest-blue'
-        aria-label="A place for robots to meet"
+        aria-label="Enter your robo friend name"
         type='search' 
         placeholder='search robots'
         onChange={searchChange}
